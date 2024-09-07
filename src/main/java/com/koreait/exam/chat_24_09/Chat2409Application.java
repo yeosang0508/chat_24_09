56e3d11ca2343989946f8c7158ab27e44f1da3ab
yeosang0508 <yeosang0508@gmail.com>
2024-09-07 17:03:06 +0900

    chat 프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/chat_24_09/Chat2409Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_24_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2409Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat2409Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/chat_24_09/Chat2409ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_24_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2409ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
